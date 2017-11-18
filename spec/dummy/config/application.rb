b239f84c0dce87fb91b77b02017427c687179ce2
Yoshikazu Kaneta <kaneta@sitebridge.co.jp>
2017-11-18 17:23:18 +0900

    Add monitor callback to verify database connection

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -3,6 +3,7 @@ require_relative 'boot'
 require 'rails/all'
 
 Bundler.require(*Rails.groups)
+require 'delayed_job_active_record' unless defined?(Delayed::Job)
 require 'delayed/master'
 
 module Dummy
