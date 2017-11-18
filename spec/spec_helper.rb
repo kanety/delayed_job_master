75493e6b4e91cfc59bfed493ca1c137dd4491f58
Yoshikazu Kaneta <kaneta@sitebridge.co.jp>
2017-11-18 20:33:18 +0900

    Add codecov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,5 +2,11 @@
 require 'simplecov'
 SimpleCov.start
 
+# codecov
+if ENV['CI'] == 'true' || ENV['CODECOV_TOKEN']
+  require 'codecov'
+  SimpleCov.formatter = SimpleCov::Formatter::Codecov
+end
+
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'delayed/master'
