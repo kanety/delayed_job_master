1974c1e4a775d9d0150b7556353a6c61b7a1b977
Yoshikazu Kaneta <kaneta@sitebridge.co.jp>
2019-09-13 23:28:53 +0900

    Remove config

--- spec/dummy/config/environments/production.rb
+++ spec/dummy/config/environments/production.rb
@@ -1,5 +1,4 @@
 Rails.application.configure do
   config.cache_classes = true
   config.eager_load = true
-  config.active_record.sqlite3.represent_boolean_as_integer = true
 end
--- spec/dummy/config/environments/test.rb
+++ spec/dummy/config/environments/test.rb
@@ -1,5 +1,4 @@
 Rails.application.configure do
   config.cache_classes = true
   config.eager_load = false
-  config.active_record.sqlite3.represent_boolean_as_integer = true
 end
