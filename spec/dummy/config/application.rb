da29ede7bea36ada6557546e918c8869345fb05f
Yoshikazu Kaneta <kaneta@sitebridge.co.jp>
2020-06-07 19:58:31 +0900

    Fix require order of dummy app for test

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -3,9 +3,9 @@ require_relative 'boot'
 require 'active_record/railtie'
 require 'active_job/railtie'
 
-Bundler.require(*Rails.groups)
 require 'delayed_job'
 require 'delayed_job_active_record'
+Bundler.require(*Rails.groups)
 
 module Dummy
   class Application < Rails::Application
