168580ff12c8391d1bcb45089989adc1e12051ca
Yoshikazu Kaneta <kaneta@sitebridge.co.jp>
2020-06-07 19:53:50 +0900

    Add test for config

---
+++ spec/delayed/master/config_spec.rb
@@ -0,0 +1,39 @@
+describe Delayed::Master::Config do
+  let(:config) do
+    Delayed::Master::Config.new
+  end
+
+  context 'master setting' do
+    it 'sets a integer value' do
+      config.monitor_wait 10
+      expect(config.monitor_wait).to eq(10)
+    end
+
+    it 'sets a boolean value' do
+      config.daemon false
+      expect(config.daemon).to eq(false)
+    end
+  end
+
+  context 'worker setting' do
+    it 'add a worker' do
+      config.add_worker do |worker|
+      end
+      expect(config.worker_settings.size).to eq(1)
+    end
+
+    it 'sets an array value' do
+      config.add_worker do |worker|
+        worker.queues %w(queue)
+      end
+      expect(config.worker_settings[0].queues).to eq(%w(queue))
+    end
+
+    it 'sets a boolean value' do
+      config.add_worker do |worker|
+        worker.exit_on_complete false
+      end
+      expect(config.worker_settings[0].exit_on_complete).to eq(false)
+    end
+  end
+end
