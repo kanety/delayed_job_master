146839ee20d98ac86149dc3a0deff72e59372c4f
Yoshikazu Kaneta <kaneta@sitebridge.co.jp>
2023-05-04 12:55:08 +0900

    Fix test

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -3,13 +3,12 @@ require_relative 'boot'
 require 'active_record/railtie'
 require 'active_job/railtie'
 
-require 'delayed_job'
-require 'delayed_job_active_record'
 Bundler.require(*Rails.groups)
 
 module Dummy
   class Application < Rails::Application
     config.active_job.queue_adapter = :delayed_job
+    config.active_record.legacy_connection_handling = false if Rails.gem_version > Gem::Version.new('6.1')
 
     database = ENV['DATABASE'] ? "database_#{ENV['DATABASE']}" : "database"
     database += "_multi" if ENV['DATABASE_CONFIG'] == 'multi'
