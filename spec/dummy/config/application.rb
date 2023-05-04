759cd9603486bb7409fb519e90654604f1a24d61
Yoshikazu Kaneta <kaneta@sitebridge.co.jp>
2023-05-04 19:20:39 +0900

    Fix config for test

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -3,6 +3,8 @@ require_relative 'boot'
 require 'active_record/railtie'
 require 'active_job/railtie'
 
+require 'delayed_job'
+require 'delayed_job_active_record'
 Bundler.require(*Rails.groups)
 
 module Dummy
