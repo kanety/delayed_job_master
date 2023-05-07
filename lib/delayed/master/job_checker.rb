73f76e422a65de2ce4b84358aceed554a593a00a
Yoshikazu Kaneta <kaneta@sitebridge.co.jp>
2023-05-07 20:25:52 +0900

    Remove needless check for new jobs

--- lib/delayed/master/job_checker.rb
+++ lib/delayed/master/job_checker.rb
@@ -18,7 +18,7 @@ module Delayed
 
         threads = @databases.map do |database|
           Thread.new(database) do |database|
-            find_jobs_in_db(database) do |setting|
+            check(database).each do |setting|
               mon.synchronize do
                 workers << Worker.new(database: database, setting: setting)
               end
@@ -33,15 +33,30 @@ module Delayed
 
       private
 
-      def find_jobs_in_db(database)
+      def check(database)
+        settings = detect_free_worker_settings(database)
+        return if settings.blank?
+
+        check_jobs(database, settings)
+      end
+
+      def detect_free_worker_settings(database)
+        workers = @master.workers.select { |worker| worker.database.spec_name == database.spec_name }
+        workers = workers.group_by(&:setting)
+        @config.worker_settings.each_with_object([]) do |setting, array|
+          free_count = setting.max_processes - workers[setting].to_a.size
+          array << [setting, free_count] if free_count > 0
+        end
+      end
+
+      def check_jobs(database, settings)
         finder = JobFinder.new(database.model)
 
-        @config.worker_settings.each do |setting|
-          count = @master.workers.count { |worker| worker.setting.queues == setting.queues }
-          slot = setting.max_processes - count
-          if slot > 0 && (job_ids = finder.call(setting, slot)).size > 0
-            [slot, job_ids.size].min.times do
-              yield setting
+        settings.each_with_object([]) do |(setting, free_count), array|
+          job_ids = finder.call(setting, free_count)
+          if job_ids.size > 0
+            [free_count, job_ids.size].min.times do
+              array << setting
             end
           end
         end
