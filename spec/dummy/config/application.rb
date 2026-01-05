afa8e8a77124e140914cbd02458527385a0d66f5
Yoshikazu Kaneta <kaneta@sitebridge.co.jp>
2026-01-05 17:18:22 +0900

    Fix test for NameError with rails < 7.2 and delayed_job 4.2

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -13,6 +13,12 @@ module Dummy
   class Application < Rails::Application
     config.load_defaults Rails::VERSION::STRING.to_f
 
+    # fix "NameError: uninitialized constant ActiveJob::QueueAdapters::AbstractAdapter" with rails < 7.2 and delayed_job 4.2
+    if ActiveJob.version < Gem::Version.new('7.2.0')
+      spec = Gem::Specification.find_by_name('activejob')
+      require File.join(spec.gem_dir, 'lib/active_job/queue_adapters/delayed_job_adapter')
+    end
+
     config.active_job.queue_adapter = :delayed_job
 
     database = ENV['DATABASE'] ? "database_#{ENV['DATABASE']}" : "database"
